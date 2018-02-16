b05c9f46fedbba73736f42e945fa7616605d12b7
tbess <timothy.bess@ignitionone.com>
2018-02-15 23:18:41 -0500

    * tear out argparse and replace with getopts * prepare to handle Stdin with input enum. I would have rather used a trait object, but tail doesn't behave normally when Stdin is used so enum is the best choice in this case.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate inotify;
-extern crate argparse;
+extern crate getopts;
 
 use std::path::Path;
 use std::iter::Iterator;
@@ -7,87 +7,63 @@ use std::fs::{File, Metadata};
 use std::collections::{HashMap, VecDeque};
 use std::io::{Read, BufRead, Seek, BufReader, SeekFrom};
 use inotify::{Inotify, WatchMask, EventMask};
-use argparse::{ArgumentParser, Print, Collect, StoreTrue};
+use getopts::Options;
 
 #[allow(dead_code)]
-static MAN_PAGE: &'static str = r#"
-NAME
-       tail - output the last part of files
+static USAGE: &'static str = r#"Usage: tail [OPTION]... [FILE]...
+Print the last 10 lines of each FILE to standard output.
+With more than one FILE, precede each with a header giving the file name.
 
-SYNOPSIS
-       tail [OPTION]... [FILE]...
+With no FILE, or when FILE is -, read standard input.
 
-DESCRIPTION
-       Print the last 10 lines of each FILE to standard output.  With more than one FILE, precede each
-       with a header giving the file name.
+Mandatory arguments to long options are mandatory for short options too.
+  -c, --bytes=[+]NUM       output the last NUM bytes; or use -c +NUM to
+                             output starting with byte NUM of each file
+  -f, --follow[={name|descriptor}]
+                           output appended data as the file grows;
+                             an absent option argument means 'descriptor'
+  -F                       same as --follow=name --retry
+  -n, --lines=[+]NUM       output the last NUM lines, instead of the last 10;
+                             or use -n +NUM to output starting with line NUM
+      --max-unchanged-stats=N
+                           with --follow=name, reopen a FILE which has not
+                             changed size after N (default 5) iterations
+                             to see if it has been unlinked or renamed
+                             (this is the usual case of rotated log files);
+                             with inotify, this option is rarely useful
+      --pid=PID            with -f, terminate after process ID, PID dies
+  -q, --quiet, --silent    never output headers giving file names
+      --retry              keep trying to open a file if it is inaccessible
+  -s, --sleep-interval=N   with -f, sleep for approximately N seconds
+                             (default 1.0) between iterations;
+                             with inotify and --pid=P, check process P at
+                             least once every N seconds
+  -v, --verbose            always output headers giving file names
+  -z, --zero-terminated    line delimiter is NUL, not newline
+      --help     display this help and exit
+      --version  output version information and exit
 
-       With no FILE, or when FILE is -, read standard input.
+NUM may have a multiplier suffix:
+b 512, kB 1000, K 1024, MB 1000*1000, M 1024*1024,
+GB 1000*1000*1000, G 1024*1024*1024, and so on for T, P, E, Z, Y.
 
-       Mandatory arguments to long options are mandatory for short options too.
-
-       -c, --bytes=[+]NUM
-              output the last NUM bytes; or use -c +NUM to output starting with byte NUM of each file
-
-       -f, --follow[={name|descriptor}]
-              output appended data as the file grows;
-
-              an absent option argument means 'descriptor'
-
-       -F     same as --follow=name --retry
-
-       -n, --lines=[+]NUM
-              output the last NUM lines, instead of the last 10; or use -n  +NUM  to  output  starting
-              with line NUM
-
-       --max-unchanged-stats=N
-              with --follow=name, reopen a FILE which has not
-
-              changed  size  after  N (default 5) iterations to see if it has been unlinked or renamed
-              (this is the usual case of rotated log files); with inotify, this option is rarely  use‐
-              ful
-
-       --pid=PID
-              with -f, terminate after process ID, PID dies
-
-       -q, --quiet, --silent
-              never output headers giving file names
-
-       --retry
-              keep trying to open a file if it is inaccessible
-
-       -s, --sleep-interval=N
-              with  -f,  sleep for approximately N seconds (default 1.0) between iterations; with ino‐
-              tify and --pid=P, check process P at least once every N seconds
-
-       -v, --verbose
-              always output headers giving file names
-
-       -z, --zero-terminated
-              line delimiter is NUL, not newline
-
-       --help display this help and exit
-
-       --version
-              output version information and exit
-
-       NUM may have a multiplier suffix: b 512, kB  1000,  K  1024,  MB  1000*1000,  M  1024*1024,  GB
-       1000*1000*1000, G 1024*1024*1024, and so on for T, P, E, Z, Y.
-
-       With  --follow (-f), tail defaults to following the file descriptor, which means that even if a
-       tail'ed file is renamed, tail will continue to track its end.  This  default  behavior  is  not
-       desirable  when  you  really want to track the actual name of the file, not the file descriptor
-       (e.g., log rotation).  Use --follow=name in that case.  That causes tail  to  track  the  named
-       file in a way that accommodates renaming, removal and creation.
-
-AUTHOR
-       Written by Timothy Bess
-
+With --follow (-f), tail defaults to following the file descriptor, which
+means that even if a tail'ed file is renamed, tail will continue to track
+its end.  This default behavior is not desirable when you really want to
+track the actual name of the file, not the file descriptor (e.g., log
+rotation).  Use --follow=name in that case.  That causes tail to track the
+named file in a way that accommodates renaming, removal and creation.
 "#;
 
 enum ModificationType {
     Added,
     Removed,
     NoChange,
+}
+
+enum Input {
+    File(File),
+    Stdin(std::io::Stdin)
 }
 
 #[derive(Debug)]
@@ -139,22 +115,41 @@ impl StatefulFile {
     }
 }
 
+fn print_usage() {
+    print!("{}", USAGE);
+    std::process::exit(0);
+}
+
 fn main() {
-    let mut file_names: Vec<String> = Vec::new();
-    let mut follow_opt = false;
-    {
-        let mut parser = ArgumentParser::new();
-        parser.set_description("Print the last 10 lines of each FILES to standard output.  With more than one FILES, precede each \
-                                with a header giving the file name. \
-                                With no FILES, or when FILES is -, read standard input.");
-        parser.add_option(&["-V", "--version"],
-                          Print(env!("CARGO_PKG_VERSION").to_string()), "Print version");
-        parser.refer(&mut follow_opt).add_option(&["-f", "--follow"],
-                                                 StoreTrue, "Output appended data as the file grows");
-        parser.refer(&mut file_names)
-            .add_argument("files", Collect, "FILES");
-        parser.parse_args_or_exit();
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("c", "bytes", "output the last NUM bytes", "NUM");
+    opts.optflag("f", "follow", "output appended as the file grows");
+    opts.optflag("F", "", "same as follow with --retry");
+    opts.optopt("n", "lines", "output the last NUM lines, instead of the last 10", "NUM");
+    opts.optflag("h", "help", "print this help menu");
+//    opts.optmulti("", "", "files to tail", "[FILE]");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+//        print!("{}", opts.usage(""));
+        print_usage();
     }
+
+    if matches.free.is_empty() {
+        eprintln!("Error: Must have at least one file in arguments");
+        print_usage();
+    }
+
+
+    let follow_opt = matches.opt_present("f");
+    let file_names: Vec<String> = matches.free;
+
     let mut watcher = Inotify::init().expect("Inotify failed to initialize");
     let mut files = HashMap::new();
     for file_name in file_names {
